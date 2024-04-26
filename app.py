beabcdf1b78293cfb69b1cb6f89f6aa5153b3b1d
danielbonillaromero <159820408+danielbonillaromero@users.noreply.github.com>
2024-04-26 13:21:32 +0000

    Yes

---
+++ app.py
@@ -0,0 +1,21 @@
+myString = 'Python is fun!'
+print(len(myString))
+
+temperature = 82
+
+if temperature > 0 :
+    print('temperature is positive')
+
+grade = 12
+
+if grade > 90:
+    print("You got an A!")
+elif grade > 80:
+    print("You got a B!")
+elif grade > 70:
+    print("You got a C!")
+elif grade > 60:
+    print("You got a D!")
+else:
+    print("You got a F")
+
